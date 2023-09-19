2b1c0ef3557cd1180592c448ce4d9f356404e5f6
koloxiya <z786849296@outlook.com>
2023-09-19 19:09:45 +0800

    ble connected

--- MainActivity.cs
+++ MainActivity.cs
@@ -1,16 +1,27 @@
+using Android.Bluetooth;
 using Android.Content;
 using Android.Hardware;
 using Android.Provider;
+using Android.Util;
+using Plugin.BLE;
+using Plugin.BLE.Abstractions;
+using Plugin.BLE.Abstractions.Contracts;
 
 namespace sensor_brightness
 {
     [Activity(Label = "@string/app_name", MainLauncher = true)]
     public class MainActivity : Activity, ISensorEventListener
     {
+        const int REQUEST_ENABLE_BT = 1;
+
         private SensorManager sensors;
         private Sensor light;
+        public IBluetoothLE ble;
+        public IDevice desktop;
 
-        private TextView text_light, text_brightness;
+        private TextView text_light, text_brightness, text_device;
+        private Spinner spinner_devices;
+
         public float lux { set; get; }
         protected override void OnCreate(Bundle? savedInstanceState)
         {
@@ -23,6 +34,36 @@ namespace sensor_brightness
             light = sensors.GetDefaultSensor(SensorType.Light);
             text_light = FindViewById<TextView>(Resource.Id.light_value);
             text_brightness = FindViewById<TextView>(Resource.Id.brightness);
+            text_device = FindViewById<TextView>(Resource.Id.device_name);
+
+            ble = CrossBluetoothLE.Current;
+            if (!ble.IsOn)
+            {
+                Intent enableBtIntent = new Intent(BluetoothAdapter.ActionRequestEnable);
+                StartActivityForResult(enableBtIntent, REQUEST_ENABLE_BT);
+            }
+            else
+            {
+                while (ble.Adapter.GetSystemConnectedOrPairedDevices().Count == 0) ;
+                desktop = ble.Adapter.GetSystemConnectedOrPairedDevices().First();
+                ble.Adapter.ConnectToDeviceAsync(desktop);
+                text_device.Text = desktop.Name;
+            }
+            ble.StateChanged += (s, e) =>
+            {
+                switch (e.NewState) 
+                {
+                case BluetoothState.On:
+                    while (ble.Adapter.GetSystemConnectedOrPairedDevices().Count == 0) ;
+                    desktop = ble.Adapter.GetSystemConnectedOrPairedDevices().First();
+                    ble.Adapter.ConnectToDeviceAsync(desktop);
+                    text_device.Text = desktop.Name;
+                    break;
+                case BluetoothState.Off:
+                    desktop = null;
+                    break;
+                }
+            };
         }
 
         public void OnAccuracyChanged(Sensor sensor, SensorStatus accuracy)
@@ -54,4 +95,5 @@ namespace sensor_brightness
         {
             return Settings.System.GetInt(this.ContentResolver, Settings.System.ScreenBrightness);
         }
+    }
 }
